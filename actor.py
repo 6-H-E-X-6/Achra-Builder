0f669845ecd3de35e5bc3a887dbb976565cc3c03
6-H-E-X-6 <ChildOfTheCity@proton.me>
2024-09-02 16:21:12 -0400

    Added update functions for culture, class, and religion

--- actor.py
+++ actor.py
@@ -4,7 +4,7 @@
 import game_data
 import json
 
-class Actor:
+class ActorModel:
     def __init__(self, culture, archetype, deity):
         self.active_skill_trees = []
         self.culture = game_data.culture_dict[culture]
@@ -17,16 +17,40 @@ class Actor:
         self.speed = 0
         self.glory = 1
         self.skill_points = 9
-        self.add_base_attributes(self.culture)
-        self.add_base_attributes(self.archetype)
-        self.add_base_attributes(self.deity)
+        self.set_base_attributes(self.culture)
+        self.set_base_attributes(self.archetype)
+        self.set_base_attributes(self.deity)
 
-    def add_base_attributes(self, character_creation_option):
+
+    # Either edit this function or make a new
+    # set of functions for updating the stats
+    # when either race, archetype, or class are changed
+    def set_base_attributes(self, character_creation_option):
         self.strength += character_creation_option.str_bonus
         self.dexterity += character_creation_option.dex_bonus
         self.willpower += character_creation_option.wil_bonus
         self.vigor += character_creation_option.vig_bonus
         self.speed += character_creation_option.speed_bonus
+
+    def update_base_attributes(self, new_option, current_option):
+        self.strength += (new_option.str_bonus - current_option.str_bonus)
+        self.dexterity += (new_option.dex_bonus - current_option.dex_bonus)
+        self.willpower += (new_option.wil_bonus - current_option.wil_bonus)
+        self.vigor += (new_option.vig_bonus - current_option.vig_bonus)
+        self.speed += (new_option.speed_bonus - current_option.speed_bonus)
+
+    def change_culture(self, new_culture):
+        self.update_base_attributes(new_culture, self.culture)
+        self.culture = new_culture
+
+    def change_archetype(self, new_archetype):
+        self.update_base_attributes(new_archetype, self.archetype)
+        self.archetype = new_archetype
+
+    def change_deity(self, new_deity):
+        self.update_base_attributes(new_deity, self.deity)
+        self.deity = new_deity
+        
 
     def level_up(self, attribute):
         match attribute:
@@ -41,31 +65,18 @@ class Actor:
                 self.vigor += 75
 
         self.skill_points += 1
+        self.glory += 1
 
-my_actor = Actor('Stran', 'Amir', 'Ashem')
-print(f'{my_actor.culture.name} {my_actor.archetype.name} of {my_actor.deity.name}')
-print(f'base STR: {my_actor.strength}, base DEX: {my_actor.dexterity}, base WIL: {my_actor.willpower}',
-      f'base vigor: {my_actor.vigor}, base speed: {my_actor.speed}')
 
-# THIS IS DRIVER CODE TO TEST THE MODULE
+# Default TEST case to make sure none
+# of the underlying business logic
+# has been broken.
+def main():
+    my_actor = ActorModel('Stran', 'Amir', 'Ashem')
+    my_actor.change_culture(game_data.culture_dict['Lochra'])
+    print(f'{my_actor.culture.name} {my_actor.archetype.name} of {my_actor.deity.name}')
+    print(f'base STR: {my_actor.strength}, base DEX: {my_actor.dexterity}, base WIL: {my_actor.willpower}',
+          f'base vigor: {my_actor.vigor}, base speed: {my_actor.speed}')
 
-# with open('tables/Table_Gods.json') as table:
-#     deity_table = json.load(table)
-#     deity_dict = {deity : game_data.Archetype_Or_Religion(deity_table[deity]) for deity in deity_table}
-# with open('tables/Table_Classes.json') as table:
-#     archetype_table = json.load(table)
-#     archetype_dict = {archetype : game_data.Archetype_Or_Religion(archetype_table[archetype]) for archetype in archetype_table}
-# with open('tables/Table_Races.json') as table:
-#     cultures_table = json.load(table)
-#     cultures_dict = {culture : game_data.Race(cultures_table[culture]) for culture in cultures_table}
-
-# my_test_actor = Actor()
-# my_test_actor.add_attributes(cultures_dict['Stran'])
-# my_test_actor.add_attributes(archetype_dict['Amir'])
-# my_test_actor.add_attributes(deity_dict['Ashem'])
-
-# print(f'Strength = {my_test_actor.strength}')
-# print(f'Dexterity = {my_test_actor.dexterity}')
-# print(f'Willpower = {my_test_actor.willpower}')
-# print(f'Vigor = {my_test_actor.vigor}')
-# print(f'Speed = {my_test_actor.speed + (my_test_actor.dexterity * 2)}')
+if __name__ == '__main__':
+    main()
